333152710817e113e917e74c3cda80031e54424d
StoreWithASombrero <storelliesteban25@gmail.com>
2025-08-15 16:33:31 +0200

    New tileset and other

--- singletons/room_manager.gd
+++ singletons/room_manager.gd
@@ -5,8 +5,9 @@ var rooms: Array[String] = [
 	"res://scenes/rooms/beginning.tscn",
 	"res://scenes/rooms/sandwiched.tscn",
 	"res://scenes/rooms/surrounded.tscn",
-	"res://scenes/rooms/bad_situation.tscn",
-	"res://scenes/rooms/new_friend.tscn"
+	"res://scenes/rooms/cornered.tscn",
+	"res://scenes/rooms/new_friend.tscn",
+	"res://scenes/rooms/plus_sign.tscn"
 ]
 
 func change_room():
