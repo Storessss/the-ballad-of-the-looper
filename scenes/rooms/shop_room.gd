f79c4ce5ded5eecdaf070735f75842988960558e
Storessss <storelliesteban25@gmail.com>
2026-01-11 21:38:29 +0100

    Most of dialogue

--- scenes/rooms/shop_room.gd
+++ scenes/rooms/shop_room.gd
@@ -4,8 +4,8 @@ func _ready() -> void:
 	super._ready()
 	
 	if GlobalVariables.fight_room_index == 4:
-		$Abbie/DialogueTrigger.event = "abbie1_1"
+		$Abbie/DialogueTrigger.event = "abbie1"
 	elif GlobalVariables.fight_room_index == 8:
-		$Abbie/DialogueTrigger.event = "abbie2_1"
+		$Abbie/DialogueTrigger.event = "abbie2"
 	elif GlobalVariables.fight_room_index == 12:
-		$Abbie/DialogueTrigger.event = "abbie3_1"
+		$Abbie/DialogueTrigger.event = "abbie3"
